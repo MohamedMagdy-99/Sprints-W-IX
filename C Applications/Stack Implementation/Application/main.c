ec963b567bd6046288aafea028757baf73abed64
Mohamed Magdy <mohamedmagdy.elkholy@gmail.com>
2021-07-30 21:13:44 +0200

    Stack Implementation Added

---
+++ C Applications/Stack Implementation/Application/main.c
@@ -0,0 +1,18 @@
+/*****************************************************************************
+* Module: Main Module
+* File Name: main.c
+* Description: Source file for Main Module
+* Author: Mohamed Magdy
+* Date: 11-July-2021
+******************************************************************************/
+#include "Stack.h"
+
+
+int main()
+{
+
+     printf("%s\n", balancedParentheses("{{(123)}}"));
+
+    return E_OK;
+}
+
