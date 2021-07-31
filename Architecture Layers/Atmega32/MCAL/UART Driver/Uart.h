ecce3bc32706fdae4fdf91047da6876dbd880531
Mohamed Magdy <mohamedmagdy.elkholy@gmail.com>
2021-07-31 14:43:31 +0200

    Atmega32 UART Driver Added

---
+++ Architecture Layers/Atmega32/MCAL/UART Driver/Uart.h
@@ -0,0 +1,72 @@
+/*****************************************************************************
+* Module: Uart Module
+* File Name: Uart.h
+* Description: Header file for UART Module
+* Author: Mohamed Magdy
+* Date: 19-July-2021
+******************************************************************************/
+
+#ifndef UART_H_
+#define UART_H_
+
+
+/* INCLUDES */
+
+#include "../../Microcontroller/Std_Types.h"
+#include "../../Microcontroller/Common_Macros.h"
+#include "../../Microcontroller/Uart_Regs.h"
+#include "Uart_Cfg.h"
+#include "../Interrupt Driver/ISR.h"
+
+/* CONSTANTS */
+#define END_OF_STRING '\0'
+#define NEW_LINE	  '\r'
+#define BACK_SPACE	  '\b'
+/*- PRIMITIVE TYPES ----------------------------------------*/
+typedef void (*pfUart_CallBack_t)(void);
+
+/*- ENUMS --------------------------------------------------*/
+typedef enum
+{
+UART_STATUS_ERROR_NOK,
+UART_STATUS_ERROR_OK
+
+}enuUart_Status_t;
+
+typedef enum
+{
+UART_INITIALIZED,
+UART_NOT_INITIALIZED
+}enuUart_initStatus_t;
+
+
+
+/*- STRUCTS AND UNIONS -------------------------------------*/
+typedef struct
+{
+	uint8_t u8_parity;
+	uint8_t u8_stopBits;
+	uint8_t u8_dataSize;
+	uint32_t u32_baudRate;
+	uint8_t u8_uartRate; 
+}strUart_Config_t;
+
+/*************************/
+/*- FUNCTION DECLARATIONS ----------------------------------*/
+enuUart_Status_t Uart_init(void);
+enuUart_Status_t Uart_syncTransmit_oneFrame(uint8_t u8_TxData);
+enuUart_Status_t Uart_syncReceive_oneFrame(uint8_t* u8_RxData);
+
+enuUart_Status_t Uart_syncTransmit_array(uint8_t* u8_TxArray, uint16_t u16_arraySize);
+enuUart_Status_t Uart_syncTransmit_string(uint8_t* u8_TxString);
+
+enuUart_Status_t Uart_syncReceive_string_vTerminal(uint8_t* u8_RxString);
+
+enuUart_Status_t Uart_asyncReceive(pfUart_CallBack_t FunToBeCalledInISR);
+enuUart_Status_t Uart_asyncReceive_Stop();
+
+void setUartRxCallback(pfUart_CallBack_t FunToBeCalledInISR);
+/* configuration variable */
+extern strUart_Config_t strUart_Config;
+
+#endif /* UART_H_ */
