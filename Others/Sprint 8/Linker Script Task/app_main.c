b7c82efa87594d07f93722dee6be0bf65d56de9b
Mohamed Magdy <mohamedmagdy.elkholy@gmail.com>
2021-09-21 22:14:21 +0200

    Added Linker Script File Test (const array added to flash at specific address)

---
+++ Others/Sprint 8/Linker Script Task/app_main.c
@@ -0,0 +1,26 @@
+#include "app_main.h"
+
+char u8InitGLobalVar = 2;
+static long int u32NonInitStaticGlobalVar; 
+const short int u16ConstGlobalVar = 3;
+
+volatile const int SprintsArray[10] __attribute__ ((section (".sprintsW9"))) = {1,2,3,4,5,5,4,3,2,1};
+
+void main(void)
+{
+	short int u16LocVar = 4;
+	static char u8InitStaticLocVar = 5;
+	if(u16ConstGlobalVar == 3)
+	{
+		u32NonInitStaticGlobalVar = 6;
+		vidFunc(&u16LocVar);
+	}
+	else
+	{
+		u32NonInitStaticGlobalVar = 7;
+	}
+	while(1)
+	{
+	
+	}
+}
